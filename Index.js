7071ecea3065c5917700349b934bc0d19b21c507
ashikmahmud <ashikmamud187@gmail.com>
2022-11-12 04:02:30 +0600

    again reinstall all the things

--- Index.js
+++ Index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { Server } = require("socket.io");
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 /* init middle wares */
 app.use(cors());
 
@@ -69,7 +69,7 @@ app.get("/", (req, res) => {
 
 
 /* listen port */
-http.listen(port, () => {
+http.listen(PORT, () => {
   console.log("listening on *:5000");
 });
 
