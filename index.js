3e100f0bc710ad1bd85f9713401c6c9b3939ecce
ashikmahmud <ashikmamud187@gmail.com>
2022-11-12 23:19:06 +0600

    try to fix

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { Server } = require("socket.io");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const PORT = process.env.PORT || 5000;
 /* init middle wares */
 app.use(cors());
