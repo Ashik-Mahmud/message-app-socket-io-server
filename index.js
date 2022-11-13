a6a6367dec33d8970d3dbb46e6f836d9607b0886
ashikmahmud <ashikmamud187@gmail.com>
2022-11-13 19:15:00 +0600

    added some extra cool features

--- index.js
+++ index.js
@@ -20,59 +20,59 @@ const io = new Server(http, {
 io.on("connection", async (socket) => {
   console.log("New user connected - " + socket.id);
 
-  const users = [];
   /* join room */
   socket.on("join_room", async (data) => {
-    socket.join(data.roomId);
-    console.log(socket.id + " User joined room: " + data.roomId);
-    users.push({ id: socket.id, roomId: data.roomId });
-   
+    socket.join(data.roomId.trim().toUpperCase());
+    console.log(
+      socket.id + " User joined room: " + data.roomId.trim().toUpperCase()
+    );
+  });
+
+  /* notify if someone join */
+  socket.on("notify_join", async (data) => {
+    socket.to(data?.roomId.trim().toUpperCase()).emit("notify_joined", {
+      roomId: data?.roomId.trim().toUpperCase(),
+      name: data?.yourName,
+      userId: socket.id,
+    });
   });
 
   /* send message  */
   socket.on("send_message", (data) => {
     console.log("Message received: " + data.message);
     socket
-      .to(data.room)
+      .to(data.room.trim().toUpperCase())
       .emit("receive_message", { ...data, sender: socket.id });
   });
 
   /* leave room */
   socket.on("leave_room", (data) => {
-    socket.leave(data.roomId);
-    console.log(socket.id + " User left room: " + data.roomId);
-    users.splice(
-      users.findIndex((user) => user.userId === socket.id),
-      1
+    socket.leave(data.roomId.trim().toUpperCase());
+    console.log(
+      socket.id + " User left room: " + data.roomId.trim().toUpperCase()
     );
-    socket.emit("users", users);
   });
 
-  /* get all the joined clients in room */
-    socket.on("get_joined_users", (data) => {
-        var clients = socket.sockets?.clients(data?.roomId); 
-        console.log(clients);
-       
+  /* notify if someone join */
+  socket.on("notify_leave", async (data) => {
+    socket.to(data?.roomId.trim().toUpperCase()).emit("notify_left", {
+      roomId: data?.roomId.trim().toUpperCase(),
+      name: data?.yourName,
+      userId: socket.id,
     });
-
-
+  });
 
   socket.on("disconnect", () => {
     console.log("User was disconnected" + socket.id);
   });
 });
 
-
-
-
 /* First Routes */
 app.get("/", (req, res) => {
   res.send({ message: "Welcome to Socket.io Chat App" });
 });
 
-
 /* listen port */
 http.listen(PORT, () => {
   console.log("listening on *:5000");
 });
-
